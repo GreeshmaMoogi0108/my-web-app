74cb7b0685fe84f81ddaa81f9a8ea81cc61751c8
Greeshmamoogi <greeshmamoogi@gmail.com>
2024-12-29 21:23:24 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple JavaScript to show an alert when the button is clicked
+document.getElementById('alertButton').addEventListener('click', function() {
+  alert('Hello! This is a simple alert.');
+});
